acfa18d47877a2969baed6c6ccb45bded9352528
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-08 08:05:40 -0500

    modify file_name * description

--- models/book.py
+++ models/book.py
@@ -11,4 +11,4 @@ class Book(BaseModel, Base):
     id = Column(String(60), primary_key=True)
     category_id = Column(String(60), ForeignKey('categories.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    file_name = Column(String(128), nullable=True)
+    description = Column(String(128), nullable=True)
