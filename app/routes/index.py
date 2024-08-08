2342fdf4bb04c3a527a202c9aa3f597e64776c1f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-08 12:27:33 -0500

    create routes

---
+++ app/routes/index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" objects that handle all default Categories"""
+from models.category import Category
+from models import storage
+from app.routes import app_routes
+from flask import abort, jsonify, make_response, request, render_template, redirect, url_for
+
+
+@app_routes.route('/')
+def login():
+    return render_template("login.html")
+
+@app_routes.route('/registro')
+def register():
+    return render_template('registro.html')
+
+@app_routes.route('/main')
+def index():
+    return render_template('main.html')
+
+@app_routes.route('/admin')
+def get_categories():
+    return render_template("admin.html")
+
+@app_routes.route("/books")
+def get_books():
+
+    return render_template("books.html")
